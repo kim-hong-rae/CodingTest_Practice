30253b23ab682e4dc88e3758f1ac5a3b59e7284f
kim-hong-rae <102844071+kim-hong-rae@users.noreply.github.com>
2024-03-18 02:18:54 +0900

    [level 1] Title: 소수 찾기, Time: 2888.45 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,12 @@
+import math
+
+def solution(n):
+    answer = 0
+    for i in range(2, n+1) :
+        for j in range(2, int(math.sqrt(i)+1)) : 
+            if(i % j == 0):
+                break;
+                print(i, j)
+        else:    
+         answer = answer+1
+    return answer
